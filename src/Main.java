f4094b0937682d1bdcdf4d16655c8aa4c6106c27
Захаров Анатолий Анатольевич <anazakharov@phoenixit.ru>
2024-08-23 16:13:46 +0300

    Second manual commit

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Manual merge request");
+        System.out.println("Second manual merge request");
     }
 }
