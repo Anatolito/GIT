77bcfc29d2c76ad93cda4507da7268d461c25b92
Захаров Анатолий Анатольевич <anazakharov@phoenixit.ru>
2024-08-23 15:33:42 +0300

    Handle giting

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,6 @@
 public class Main {
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Hi");
-        System.out.println("Add origin");
-        System.out.println("Before Add branch");
-        System.out.println("Branch different");
-        System.out.println("Another branch different");
-        System.out.println("Branch 2 different");
-        System.out.println("Different from github");
-        System.out.println("Second Different from github");
-        System.out.println("Master different");
-        System.out.println("feature");
-        System.out.println("Master change");
-        System.out.println("b");
-        System.out.println("m");
-        System.out.println("b1");
-        System.out.println("m1");
-        System.out.println("b2");
-        System.out.println("New age");
-        System.out.println("Second New Age");
-        System.out.println("Git add all");
-        System.out.println("Git fetch");
-        System.out.println("Git fetch2");
-        System.out.println("git branch");
-        System.out.println("Change from server");
-        System.out.println("Change for all update");
-        System.out.println("Git pull = git fetch + git merge");
-        System.out.println("Git pull = git fetch + git merge 2");
-        System.out.println("Change for all update");
-
     }
 }
